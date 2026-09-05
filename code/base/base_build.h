9098b831f46f3685d64401aecf8b2097524ce904
Raymaekers Luca <luca@keyfried.com>
2026-09-05 15:56:52 +0200

    checkpoint

---
+++ code/base/base_build.h
@@ -0,0 +1,26 @@
+/* date = March 17th 2026 1:04 pm */
+#ifndef BASE_BUILD_H
+#define BASE_BUILD_H
+
+//- Base 
+#define BASE_PROFILE 0
+
+//- Platform 
+#define RL_PLATFORM_COLEMAK 1
+#define RL_PLATFORM_INTERNAL 1
+#define RL_PLATFORM_DEBUG_UI 1
+#define RL_PLATFORM_HOT_RELOAD_SHADERS 1
+#define RL_PLATFORM_FORCE_X11 0
+#define RL_PLATFORM_FORCE_SMALL_RESOLUTION 1
+#define RL_PLATFORM_FORCE_UPDATE_HZ 165
+
+//- Editor 
+#define EDITOR_INTERNAL 1
+#define EDITOR_HOT_RELOAD_SHADERS 1
+
+//- Muze 
+#define MUZE_INTERNAL 1
+#define MUZE_HOT_RELOAD_SHADERS 1
+#define MUZE_STARTUP_PROFILE 0
+
+#endif // BASE_BUILD_H
