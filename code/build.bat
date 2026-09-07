70f792269190a86a8fb222d0632fa6fb5f2d1925
Raymaekers Luca <luca@keyfried.com>
2026-09-07 09:21:51 +0200

    Added overridable build parameters

--- code/build.bat
+++ code/build.bat
@@ -8,11 +8,11 @@ cd %~dp0
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 
-IF NOT EXIST ..\build\cling.exe (
+IF NOT EXIST .\cling.exe (
   cl -MTd -Gm- -nologo -GR- -EHa- -Oi -FC -Zi -WX -W4 -wd4459 -wd4456 -wd4201 -wd4100 -wd4101 -wd4189 -wd4063 -wd4505 -wd4996 -wd4389 -wd4244 -wd4702 -wd5287 -I..\code -Zc:strictStrings- -I..\code -Fecling.exe -Fmcling.map ..\code\cling\cling.c /link -opt:ref -incremental:no
 )
 
 SET CLING_NOREBUILD=1
-..\build\cling.exe
+.\cling.exe
 
 REM ----------------------------------------------------------
